a555355d69a4fe9cd01d8bea86ce5b057cd1cd66
Eltrac <hi@guhub.cn>
2025-11-26 09:54:49 +0800

    feat: implement minimal router

--- context.go
+++ context.go
@@ -1,1 +1,21 @@
 package growel
+
+import (
+	"net/http"
+)
+
+type Context struct {
+	W      http.ResponseWriter
+	R      http.Request
+	Params map[string]string
+}
+
+type Handler func(c *Context)
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r http.Request) {
+	h(&Context{
+		W:      w,
+		R:      r,
+		Params: make(map[string]string),
+	})
+}
--- router.go
+++ router.go
@@ -1,1 +1,60 @@
 package growel
+
+import (
+	"strings"
+)
+
+type route struct {
+	method  string
+	parts   []string
+	handler Handler
+}
+
+type Router struct {
+	routes []route
+}
+
+func NewRouter() *Router {
+	return &Router{}
+}
+
+func (r *Router) Add(method, path string, h Handler) {
+	parts := strings.Split(path, "/")
+	r.routes = append(r.routes, route{method, parts, h})
+}
+
+func (r *Router) Find(method, path string) (Handler, map[string]string) {
+	parts := strings.Split(path, "/")
+
+	for _, rt := range r.routes {
+		// match HTTP method
+		if rt.method != method {
+			continue
+		}
+
+		// match path part number
+		if len(rt.parts) != len(parts) {
+			continue
+		}
+
+		// compare each segment
+		params := make(map[string]string)
+		matched := true
+		for i, seg := range rt.parts {
+			// parse dynamic parameter
+			if strings.HasPrefix(seg, ":") {
+				params[seg[1:]] = parts[i]
+			}
+
+			// static mismatch
+			if seg != parts[i] {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			return rt.handler, params
+		}
+	}
+	return nil, nil
+}
